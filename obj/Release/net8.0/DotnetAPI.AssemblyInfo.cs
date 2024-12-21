b5c807076086bde32c4edf857ebb43c7423180b9
Ernesto Gallardo <egallardodev@gmail.com>
2024-12-21 03:01:11 -0500

    Fixed GetDeparmentsInfo

--- obj/Release/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5bc2483e4139c3cb8947d723951978320139b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121cbd1d3875a1dff24da3d61f27c0b5d9673c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
